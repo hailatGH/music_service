95f92d137bdb079090053047cf31aefe8b59bb9b
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-10-04 12:16:33 +0300

    Done

--- src/player/mobileAppviews.py
+++ src/player/mobileAppviews.py
@@ -409,18 +409,18 @@ class FavouritesByUserIdViewSet(viewsets.ModelViewSet):
             for fav_count in range(len(page)):
                 page[fav_count]['fav_id'] = page[fav_count]['id']
                 track = TracksModel.objects.filter(track_status=True, id=page[fav_count]['track_id']).order_by('-created_at').values('id','track_name','track_description','track_coverImage','track_audioFile','track_lyrics','track_price','artists_featuring','artist_id','album_id','genre_id','encoder_FUI')
-                # for val in ['id','track_name','track_description','track_coverImage','track_audioFile','track_lyrics','track_price','artists_featuring','artist_id','album_id','genre_id','encoder_FUI']:
-                #     page[fav_count][val] = track[0][val]
-                page.append(track)
+                for val in ['id','track_name','track_description','track_coverImage','track_audioFile','track_lyrics','track_price','artists_featuring','artist_id','album_id','genre_id','encoder_FUI']:
+                    page[fav_count][val] = track[0][val]
+                # page.append(track)
                 artists = ArtistsModel.objects.filter(id=track[0]['artist_id'])
-                artist_name = ""
-                if artists.count() > 1:
-                    for artist_count in range(len(artists)):    
-                        artist_name = artist_name + ", " + artists.values('artist_name')[artist_count]['artist_name']
-                elif track[0]['artists_featuring'] != "":
-                    artist_name = artist_name + " ft. " + track[0]['artists_featuring']
-                else:
-                    artist_name = artists.values('artist_name')[0]['artist_name']
+                # artist_name = ""
+                # if artists.count() > 1:
+                #     for artist_count in range(len(artists)):    
+                #         artist_name = artist_name + ", " + artists.values('artist_name')[artist_count]['artist_name']
+                # elif track[0]['artists_featuring'] != "":
+                #     artist_name = artist_name + " ft. " + track[0]['artists_featuring']
+                # else:
+                artist_name = artists.values('artist_name')[0]['artist_name']
                 page[fav_count]['artist_name'] = artist_name
                 page[fav_count]['is_purchasedByUser'] = PurchasedTracksModel.objects.filter(track_id=page[fav_count]['id'], user_FUI=userId).exists()
         return Response(page)
