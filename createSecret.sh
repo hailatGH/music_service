eb0ecc0592290240eaa74e83697848641d4d6f77
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-10-04 16:52:41 +0300

    vesion_1.1 final commit

---
+++ createSecret.sh
@@ -0,0 +1,8 @@
+# gcloud sql databases create kinmusic-music-database-dev --instance=kinmusic-postgresql-v14
+
+gcloud secrets create music_service_settings_dev --replication-policy automatic
+gcloud secrets versions add music_service_settings_dev --data-file .env.dev
+
+gcloud secrets add-iam-policy-binding music_service_settings_dev \
+    --member serviceAccount:299791645258@cloudbuild.gserviceaccount.com \
+    --role roles/secretmanager.secretAccessor
