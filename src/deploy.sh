987aa82d0019e600ceef55a1a39b6be6da0302d9
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-10-05 10:57:34 +0300

    Popular tracks api endpoint completed!

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-rm test.py
+echo "Enter a commit for the push: "
+read COMMIT
+
 cd core
 mv settings.py basesettings.py
 mv cloudsettings.py settings.py
+
 cd ../../
 git add .
-git commit -m "Done"
+git commit -m "$COMMIT"
 git push --set-upstream origin main
-
 
 cd src/core/
 mv settings.py cloudsettings.py
--- src/deploy_dev.sh
+++ src/deploy_dev.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-rm test.py
+echo "Enter a commit for the push: "
+read COMMIT
+
 cd core
 mv settings.py basesettings.py
 mv cloudsettings_dev.py settings.py
+
 cd ../../
 git add .
-git commit -m "Done"
+git commit -m "$COMMIT"
 git push --set-upstream origin version_1.1
-
 
 cd src/core/
 mv settings.py cloudsettings_dev.py
