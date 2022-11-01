e2ccfa4ce227e872c52925874ae76fe3cb4be527
hailatGH <hailat.alx@gmail.com>
2022-11-01 15:28:48 +0300

    hot fix

--- src/player/mobileAppviews.py
+++ src/player/mobileAppviews.py
@@ -519,7 +519,7 @@ class PlayListsByUserIdViewSet(viewsets.ModelViewSet):
         userId = request.query_params['userId']
         data = []
         playlist = self.queryset.filter(user_FUI=userId).order_by('-created_at').values('id','playlist_name','user_FUI')
-        data[0] = playlist.count()
+        data.append(playlist.count())
         page = self.paginate_queryset(playlist)
         data.append(page)
         return Response(data)
