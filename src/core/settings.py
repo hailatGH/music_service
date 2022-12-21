d57d7ddcf556e85f13b937b97247b286f39d4fbe
hailatGH <hailat.alx@gmail.com>
2022-12-21 15:57:16 +0300

    all secrets fetch from .env file

--- src/core/settings.py
+++ src/core/settings.py
@@ -10,52 +10,37 @@ from .basesettings import *
 django.utils.encoding.force_text = force_str
 load_dotenv()
 
-# keyVaultName = os.environ["KEY_VAULT_NAME"]
-# keyVaultName = os.getenv('KEY_VAULT_NAMES')
-# KVUri = f"https://{keyVaultName}.vault.azure.net"
-
-# credential = DefaultAzureCredential()
-# client = SecretClient(vault_url=KVUri, credential=credential)
-
-# if client.get_secret("DEBUG") == 'True':
-
 DEBUG = os.getenv('DEBUG')
 SECRET_KEY = (str, os.getenv("SECRET_KEY"))
-URL = os.getenv('URL')
 
-if URL:
-    ALLOWED_HOSTS = [urlparse(URL).netloc]
-    CSRF_TRUSTED_ORIGINS = [URL]
+if os.getenv('URL'):
+    ALLOWED_HOSTS = [urlparse(os.getenv('URL')).netloc]
+    CSRF_TRUSTED_ORIGINS = [os.getenv('URL')]
 else:
     ALLOWED_HOSTS = ["*"]
 
-# ALLOWED_HOSTS = [
-#     "music-service.calmgrass-743c6f7f.francecentral.azurecontainerapps.io"]
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://music-service.calmgrass-743c6f7f.francecentral.azurecontainerapps.io/']
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "music_database",
-        'USER': "zemadatabaseadmin",
-        'PASSWORD': "StrongP@ssword",
-        'HOST': "zema-postgresql-v100.postgres.database.azure.com",
-        'PORT': '5432',
-        'OPTIONS': {'sslmode': 'disable'}
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {'sslmode': os.getenv('DB_SSL_MODE')}
     }
 }
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/static/'),
 ]
-AZURE_ACCOUNT_NAME = 'zemastroragev100'
-AZURE_ACCOUNT_KEY = 'AFsY2hZVbyYBKisEkRL+toNNJ7yBOzoJ/cruOxurFHnU84vE+Cmloq9S2ZkCxYaxrM5QemPsUiX5+ASt4WEg8w=='
+AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
+AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
-AZURE_LOCATION = 'zemacontainer'
-AZURE_CONTAINER = 'zemacontainer'
+AZURE_LOCATION = os.getenv('AZURE_LOCATION')
+AZURE_CONTAINER = os.getenv('AZURE_CONTAINER')
 
 STATIC_LOCATION = 'static'
 STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 
-STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-DEFAULT_FILE_STORAGE = 'core.custom_storage.AzureMediaStorage'
+STATICFILES_STORAGE = os.getenv('STATICFILES_STORAGE')
+DEFAULT_FILE_STORAGE = os.getenv('DEFAULT_FILE_STORAGE')
