57fbfabeec820437bdf8a008d1f4c4dd09429ec7
Sabian-A <abelteum13@gmail.com>
2023-01-25 19:07:16 +0300

    fixed signals

--- src/music/apps.py
+++ src/music/apps.py
@@ -5,9 +5,9 @@ class MusicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'music'
 
-    # def ready(self):
+    def ready(self):
     # Implicitly connect signal handlers decorated with @receiver.
-    from . import signals
+        from . import signals
     # Explicitly connect a signal handler.
     # post_save.connect(signals.update_document)
     # post_delete.connect(signals.delete_document)
