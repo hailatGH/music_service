4fde4fbb13e439f805b050311560af76fdc68b7f
hailatGH <hailat.alx@gmail.com>
2023-01-30 11:38:55 +0300

    pagination removed from tracks by encoder id

--- src/music/mobileAppviews.py
+++ src/music/mobileAppviews.py
@@ -153,7 +153,6 @@ class TracksByEncoderId(viewsets.ModelViewSet):
 
     queryset = TracksModel.objects.all()
     serializer_class = TracksSerializer
-    pagination_class = StandardResultsSetPagination
 
     def create(self, request, *args, **kwargs):
         return Response("Not Allowed")
